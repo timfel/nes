9cf8efb2ec315cb1376656723fd7fad8f74e3a2e
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-11-09 14:01:50 +1100

    Add pyjion toggle

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from nes.pycore.system import NES as pyNES
 
-# import pyjion
+import pyjion
 #run_tests()
 
-# pyjion.enable()
-# pyjion.config(graph=True, level=2)
+pyjion.enable()
+pyjion.config(graph=True, level=2)
 nes = None
-nes = pyNES("./roms/Snake.nes")
+nes = pyNES("./roms/Mario.nes")
 
 
 if nes is not None:
     nes.run()
 
+from nes.pycore.ppu import NESPPU
+
+print(pyjion.graph(NESPPU.run_cycles.__code__))
